129d22b49a2105a715d3148868009e02d58b72c7
khazarglzd <khazarguluzade@gmail.com>
2024-04-04 18:29:14 +0400

    adding sass & creating Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,24 @@
+function Sidebar() {
+
+    return (
+        <>
+            <aside className="sidebar">
+                <div className="logo">
+                    <a>Brand<b>ColorsPicker</b></a>
+                </div>
+                <div className="description">
+                    The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
+                </div>
+                <nav className="menu">
+                    <ul>
+                        <li>
+                            <a>About BrandColorsPicker</a>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+        </>
+    )
+}
+
+export default Sidebar
