938c2314273616b78e9f1b0360d7de611823fa87
khazarglzd <khazarguluzade@gmail.com>
2024-04-04 18:43:18 +0400

    creating modal for Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,14 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+import { GrClose } from 'react-icons/gr';
+
 function Sidebar() {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    const toggleModal = () => {
+        setModalIsOpen(!modalIsOpen);
+    }
 
     return (
         <>
@@ -7,16 +17,32 @@ function Sidebar() {
                     <a>Brand<b>ColorsPicker</b></a>
                 </div>
                 <div className="description">
-                    The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
+                    The biggest collection of official brand color codes around. Curated by @brandcolorspicker and friends.
                 </div>
                 <nav className="menu">
                     <ul>
                         <li>
-                            <a>About BrandColorsPicker</a>
+                            <a onClick={toggleModal}>About BrandColorsPicker</a>
                         </li>
                     </ul>
                 </nav>
             </aside>
+
+            <Modal isOpen={modalIsOpen}
+                onRequestClose={toggleModal}
+                className="about-modal"
+                overlayClassName="about-modal-overlay">
+                <button className="modal-close-btn" onClick={toggleModal}>
+                    <GrClose />
+                </button>
+                <h3>About BrandColorsPicker</h3>
+                <p>
+                    BrandColors was created by DesignBombs. The goal was to create a helpful reference for the brand color codes that are needed most often.
+                </p>
+                <p>
+                    It's been featured by Smashing Magazine, CSS-Tricks, Web Design Depot, Tuts+, and over 2 million pageviews. There are now over 600 brands with 1600 colors and the collection is always growing.
+                </p>
+            </Modal>
         </>
     )
 }
